fdeb3ff4d7de459756859ed8fc2c4c4315272089
Naama Kaplan <nkaplany@gmail.com>
2021-11-29 14:14:03 +0100

    basic html form

---
+++ friend-book/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+
+})
+export class AppComponent {
+  // title = 'friend-book';
+  languages = [
+    {value: 'css', name: 'CSS'},
+    {value: 'html', name: 'HTML'},
+    {value: 'javascript', name: 'JavaScript'},
+    {value: 'php', name: 'PHP'}
+  ];
+}
